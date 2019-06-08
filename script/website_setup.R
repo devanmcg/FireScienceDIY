9535bc3e761c7425d76aa0401157230bd36695ab
devanmcg <devan.mcgranahan@gmail.com>
2019-06-07 22:31:58 -0500

    add prelim website

---
+++ script/website_setup.R
@@ -0,0 +1,10 @@
+# Setup
+  devtools::install_github('rstudio/blogdown')
+  pacman::p_load(blogdown)
+  install_hugo()
+
+  setwd("C:/Users/Devan.McGranahan/GoogleDrive/FireScienceDIY/website")
+  new_site() 
+  install_theme("kishaningithub/hugo-creative-portfolio-theme", theme_example = TRUE, update_config = TRUE)
+  
+  
