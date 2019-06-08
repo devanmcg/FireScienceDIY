cd5284d757e695af613a6eabf2df6d002423051e
devanmcg <devan.mcgranahan@gmail.com>
2019-06-07 23:11:34 -0500

    move website to master btanch

--- script/website_setup.R
+++ script/website_setup.R
@@ -1,10 +1,8 @@
-# Setup
-  devtools::install_github('rstudio/blogdown')
   pacman::p_load(blogdown)
-  install_hugo()
 
   setwd("C:/Users/Devan.McGranahan/GoogleDrive/FireScienceDIY/website")
   new_site() 
   install_theme("kishaningithub/hugo-creative-portfolio-theme", theme_example = TRUE, update_config = TRUE)
   
   
+  blogdown::new_post("Post Title", ext = '.Rmd')
